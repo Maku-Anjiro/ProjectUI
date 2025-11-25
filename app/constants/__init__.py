772a08d83f8f5e3145571376d051f72020328110
johnpaulaquino <forgames.app2022@gmail.com>
2025-11-25 19:27:46 +0800

    .

--- app/constants/__init__.py
+++ app/constants/__init__.py
@@ -8,14 +8,3 @@ class Constants(BaseSettings):
      JWT_KEY: str
      JWT_REFRESH_EXPIRATION: int
      JWT_ALGORITHM: str
-
-     # email configurations
-     MAIL_FROM: EmailStr
-     MAIL_PASSWORD: SecretStr
-     MAIL_PORT: int
-     MAIL_STARTTLS: bool
-     MAIL_USERNAME: str
-     MAIL_SSL_TLS: bool
-     MAIL_SERVER: str
-     MAIL_USE_CREDENTIALS: bool
-     MAIL_VALIDATE_CERTS: bool
